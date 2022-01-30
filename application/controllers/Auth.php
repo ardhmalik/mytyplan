9826ec58fb8c92374b9bda593a7fb8cf9c6b4424
ardhmalik <ardhiansyahmalik1200@gmail.com>
2022-01-30 21:07:09 +0700

    Create complete code register || add new account

--- application/controllers/Auth.php
+++ application/controllers/Auth.php
@@ -35,6 +35,7 @@ class Auth extends CI_Controller
 		$auth = $this->auth_model;
 		$validation = $this->form_validation;
 		$validation->set_rules($auth->reg_rules());
+		$sessions = $this->session;
 
 		$data = [
 			'project'=>'My This Year Plan',
@@ -46,7 +47,28 @@ class Auth extends CI_Controller
 			$this->load->view('auth/register', $data);
 			$this->load->view('auth/footer');
 		} else {
-			echo "You have successfully registered!";
+			/**
+			 * add true in post param to avoid XSS attack
+			 * add htmlspecialchars() for change character to HTML entity
+			 * add password_hash() for hash password
+			 *  */ 
+			$input = [
+				'email'=>htmlspecialchars($this->input->post('email', true)),
+				'username'=>htmlspecialchars($this->input->post('username', true)),
+				'password'=>password_hash($this->input->post('password'), PASSWORD_DEFAULT)
+			];
+
+			// var_dump($input);
+			// die;
+
+			$auth->createUser($input);
+			$sessions->set_flashdata(
+				'message',
+				'<div class="alert alert-success" role="alert">
+				Your account has been registered, Please Login...
+				</div>'
+			);
+			redirect('auth/login');
 		}
 	}
 }
