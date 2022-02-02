1e39a438202f1918ce4cfa0b8bdf7ba45f6c168d
ardhmalik <ardhiansyahmalik1200@gmail.com>
2022-02-02 08:02:12 +0700

    Create route for more pages

--- application/views/sections/main.php
+++ application/views/sections/main.php
@@ -1,7 +1,20 @@
 <?php
 $this->load->view('sections/header');
 $this->load->view('sections/navbar');
-$this->load->view('pages/dashboard');
-$this->load->view('sections/modal');
+
+switch ($title) {
+    case 'Dashboard':
+        $this->load->view('pages/dashboard');
+        $this->load->view('sections/modal');
+        break;
+    case 'Logs':
+        $this->load->view('pages/logs');
+        break;
+    default:
+        $this->load->view('pages/dashboard');
+        break;
+}
+
 $this->load->view('sections/footer');
+
 ?>
