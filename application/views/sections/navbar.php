89e4d0cf258f16e7bed6581d0b2e97c7a38a93ba
ardhmalik <ardhiansyahmalik1200@gmail.com>
2022-02-02 08:00:43 +0700

    Fixed navbar layout

--- application/views/sections/navbar.php
+++ application/views/sections/navbar.php
@@ -1,16 +1,26 @@
 <!-- Navbar -->
-<nav class="navbar navbar-light bg-light mb-3">
+<nav class="navbar navbar-expand-lg navbar-light bg-light">
     <div class="container">
-        <a href="<?= site_url() ?>" class="navbar-brand">
-            <?= $user['username'] ?>
+        <a href="<?= site_url('plans/dashboard') ?>" class="navbar-brand">
+            <?= $project ?>
         </a>
-        <div class="d-flex">
-            <?php
-                date_default_timezone_set("Asia/Jakarta");
-                echo date("H:i") . " WIB"; 
-            ?>
-             | 
-            <a href="<?= site_url('auth/logout') ?>">Logout</a>
+        <div class="d-flex" id="navbarSupportedContent">
+            <ul class="navbar-nav ms-auto mb-2 mb-lg-0">
+                <li class="nav-item dropdown">
+                    <a class="nav-link dropdown-toggle" href="#" id="actionDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                        <?= $user['username'] ?>
+                    </a>
+                    <ul class="dropdown-menu" aria-labelledby="actionDropdown">
+                        <li>
+                            <a href="<?= site_url('plans/userlogs') ?>" class="dropdown-item">Logs</a>
+                        </li>
+                        <li><hr class="dropdown-divider"></li>
+                        <li>
+                            <a href="<?= site_url('auth/logout') ?>" class="dropdown-item">Logout</a>
+                        </li>
+                  </ul>
+                </li>
+            </ul>
         </div>
     </div>
 </nav>
