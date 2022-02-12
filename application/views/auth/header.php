4c5c56524546b18cb6d01cab706c3c7eb68ee402
ardhmalik <ardhiansyahmalik1200@gmail.com>
2022-02-12 20:24:23 +0700

    Change resource CDN to local

--- application/views/auth/header.php
+++ application/views/auth/header.php
@@ -8,10 +8,9 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title><?= $title . " | " . $project  ?></title>
     <!-- Bootstrap CSS -->
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.7.2/font/bootstrap-icons.css">
+    <link href="<?= site_url('/assets/css/bootstrap.min.css') ?>" rel="stylesheet">
     <!-- Font Awesome -->
-    <script src="https://kit.fontawesome.com/d514e070a0.js" crossorigin="anonymous"></script>
+    <script src="<?= site_url('/assets/js/all.min.js') ?>"></script>
 </head>
 
 <body>
--- application/views/sections/header.php
+++ application/views/sections/header.php
@@ -10,10 +10,9 @@
         <?= "$title | $project" ?>
     </title>
     <!-- Bootstrap CSS -->
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.7.2/font/bootstrap-icons.css">
+    <link href="<?= site_url('/assets/css/bootstrap.min.css') ?>" rel="stylesheet">
     <!-- Font Awesome -->
-    <script src="https://kit.fontawesome.com/d514e070a0.js" crossorigin="anonymous"></script>
+    <script src="<?= site_url('/assets/js/all.min.js') ?>"></script>
 </head>
 
 <body>
