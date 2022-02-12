4789ea60f598eca8d2366ffd731baef8edf23d3a
ardhmalik <ardhiansyahmalik1200@gmail.com>
2022-02-12 20:24:43 +0700

    Fixed grid layout

--- application/views/pages/dashboard.php
+++ application/views/pages/dashboard.php
@@ -25,7 +25,7 @@
     <!-- Content -->
     <?php foreach ($months as $mn) : ?>
         <!-- Months Card -->
-        <div class="col-sm-6 col-md-4 mb-4">
+        <div class="col-sm-6 col-lg-4 mb-4">
             <div class="card h-100 shadow-sm">
                 <!-- Card Header -->
                 <div class="card-header">
--- application/views/pages/successp.php
+++ application/views/pages/successp.php
@@ -17,7 +17,7 @@
     <!-- Content -->
     <?php foreach ($months as $mn) : ?>
         <!-- Months Card -->
-        <div class="col-sm-6 col-md-4 mb-4">
+        <div class="col-sm-6 col-lg-4 mb-4">
             <div class="card h-100 shadow-sm">
                 <div class="card-header">
                     <h4>
